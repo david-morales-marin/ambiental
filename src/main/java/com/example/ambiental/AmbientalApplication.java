807d39edf7696a6cdb14b737219c93a1f92812fb
DavidMorales <edavid.morales@udea.edu.co>
2023-02-26 20:45:24 -0500

    se agregan mejoras de los controladores

--- src/main/java/com/example/ambiental/AmbientalApplication.java
+++ src/main/java/com/example/ambiental/AmbientalApplication.java
@@ -2,8 +2,14 @@ package com.example.ambiental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories("com.example.ambiental")
+@EntityScan("com.example.ambiental")
 public class AmbientalApplication {
 
 	public static void main(String[] args) {
